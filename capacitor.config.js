c516167fadaf008703a43ef33755e4555adc1c39
rafaelsilva81 <rafaelgaldinosilva81@gmail.com>
2022-07-29 23:49:07 -0300

    Onboarding finished

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config = {
-  appId: 'io.ionic.starter',
+  appId: 'io.app-psicologos.dev',
   appName: 'app-psicologos',
   webDir: 'build',
   bundledWebRuntime: false
